dc9a26032d8cd54f60e9495d96a93f58dac0ee1f
carlssonemil <hello@emilcarlsson.se>
2020-05-02 01:34:44 +0200

    :bug: Update API URL and improve error handling

--- src/views/Banned.vue
+++ src/views/Banned.vue
@@ -1,11 +1,16 @@
 <template>
   <transition name="fade">
-    <div id="banned" class="page" v-show="banned != null">
-      <div class="banned">
+    <div id="banned" class="page" v-show="show">
+      <div class="banned" v-if="banned != null">
         <h1>Are you banned?</h1>
         <p v-if="banned">Yes. 🙄</p>
         <p v-if="!banned">No. 😅</p>
         <router-link to="/support"><eva-icon name="arrow-back" height="24" width="24" fill="#8c7ae6"></eva-icon>Go back to support</router-link>
+      </div>
+      <div v-if="error" class="error">
+        <h1>Uh-oh... 🤔</h1>
+        <p>Looks like something is wrong with the API, try again later.</p>
+        <router-link to="/"><eva-icon name="arrow-back" height="24" width="24" fill="#8c7ae6"></eva-icon>Go back to home</router-link>
       </div>
     </div>
   </transition>
@@ -19,17 +24,61 @@ export default {
   data() {
     return {
       banned: null,
+      error: null,
+      show: false
     }
   },
   mounted() {
-    axios.get(`${ process.env.VUE_APP_BASE_API_URL }blacklisted`)
-      .then(response => this.banned = response.data);
+    axios.get(`${ process.env.VUE_APP_BASE_API_URL }blacklisted/status`)
+      .then(response => {
+        this.banned = response.data;
+        this.show = true;
+      })
+      .catch(() => {
+        this.error = true;
+        this.show = true;
+      });
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/scss/variables.scss';
+
+.error {
+  padding-top: 5vh;
+  text-align: center;
+
+  h1 {
+    font-size: 50px;
+    font-weight: 600;
+  }
+
+  p {
+    font-size: 28px;
+    margin: 25px 0 50px;
+  }
+
+  a {
+    font-size: 22px;
+
+    &:hover {
+      text-decoration: none;
+      
+      i {
+        left: -5px;
+      }
+    }
+
+    i {
+      left: 0;
+      margin-right: 10px;
+      position: relative;
+      top: 4px;
+      transition: left 0.25s ease;
+    }
+  }
+}
 
 .banned {
   padding-top: 5vh;
